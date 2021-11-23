66023d52a40a6409257e58a23c18241da1930385
adioihsan <adioihsan@gmail.com>
2021-11-23 18:37:59 +0700

    Add route for login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import MainStore from "./layout/store/Main";
 import Dashboard from "./component/store/Dashboard";
 import Profile from "./component/store/Profile";
 import Home from "./component/front/Home";
+import Login from "./component/front/auth/Login";
+import Register from "./component/front/auth/Register";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/store" element={<MainStore />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="profile" element={<Profile />} />
