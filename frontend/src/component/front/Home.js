d8c0974410115107f01edafdee8dd133a3f86741
adioihsan <adioihsan@gmail.com>
2021-12-16 19:14:29 +0700

    Add promo slider to home

--- frontend/src/component/front/Home.js
+++ frontend/src/component/front/Home.js
@@ -1,5 +1,24 @@
 import MainNavbar from "../../layout/front/MainNavbar";
+import { Col, Container, Row } from "reactstrap";
+import SlidePromo from "./home/SlidePromo";
+import ChoosenCategory from "./home/ChoosenCategory";
 function Home() {
-  return <MainNavbar />;
+  return (
+    <>
+      <MainNavbar />
+      <Container className="p-3">
+        <Row className="my-3">
+          <Col sm="12">
+            <SlidePromo />
+          </Col>
+        </Row>
+        <Row className="my-3">
+          <Col sm="12">
+            <ChoosenCategory />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 export default Home;
