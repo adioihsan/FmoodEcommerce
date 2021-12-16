e25cad793a944fa3dd72dd98e851449fb56b6094
adioihsan <adioihsan@gmail.com>
2021-12-16 20:27:10 +0700

    Create database migration file for models

---
+++ backend/database/migrations/2021_12_16_124133_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("user_id");
+            $table->double("total");
+            $table->double("shipment_fee");
+            $table->integer("payment_id");
+            $table->string("status",10);
+            $table->date("required_date");
+            $table->date("shipped_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
