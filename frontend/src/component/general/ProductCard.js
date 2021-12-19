8220eecd2fbf6f0b23b1be4c95fc78e525e22faa
adioihsan <adioihsan@gmail.com>
2021-12-19 15:39:37 +0700

    Edit Product card design

--- frontend/src/component/general/ProductCard.js
+++ frontend/src/component/general/ProductCard.js
@@ -4,15 +4,18 @@ function ProductCard(props) {
   const data = props.data;
   return (
     <Card
-      style={{ height: "400px", width: "225px", fontSize: "0.9rem" }}
-      className="shadow-sm"
+      style={{ minHeight: "360px", width: "200px", fontSize: "0.9rem" }}
+      className="shadow-sm my-2 mx-2"
     >
       <CardImg src={serverUrls.storage + "/" + data.img_main} />
       <CardBody className="py-2 px-2 ">
         <div className="d-flex flex-column justify-content-around fw-lighter h-100">
-          <span id="pruduct-name">{data.name}</span>
-          <span className="fw-bold">Rp. {data.price}</span>
-          <div>
+          <span id="pruduct-name">
+            {data.name.slice(0, 50)}
+            {data.name.length > 50 ? "..." : ""}
+          </span>
+          <span className="fw-bold my-1">Rp. {data.price}</span>
+          <div className="my-1">
             {" "}
             <Badge color="danger">{data.discount_percent}%</Badge>
             <small
@@ -22,13 +25,13 @@ function ProductCard(props) {
               Rp. {data.discount_price}{" "}
             </small>
           </div>
-
-          <div style={{ fontSize: "0.8rem" }}>
+          <div style={{ fontSize: "0.8rem" }} className="my-1">
             <img
               src="/images/pin.png"
               width="16px"
               height="16px"
               className="float-start mx-1"
+              alt="origin: "
             />{" "}
             <span id="origin">{data.origin}</span>
           </div>
@@ -41,6 +44,7 @@ function ProductCard(props) {
               width="16px"
               height="16px"
               className="float-start mx-1"
+              alt="rating: "
             />{" "}
             <span id="rating"> {data.rating}</span>
             <span className="mx-1">|</span>
