a1a35c4502193be412425b4dfed78abdddebd8e0
adioihsan <adioihsan@gmail.com>
2021-12-21 15:04:07 +0700

    Created : add-cart API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\CartController;
 use App\Http\Controllers\API\ProductController;
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ Route::post('login',[AuthController::class,'login']);
 Route::get('get-products/all',[ProductController::class,'getAll']);
 Route::get('get-products/visible',[ProductController::class,'getVisible']);
 Route::get('find-product-by-name',[ProductController::class,'findProductByName']);
+Route::get('test',[ProductController::class,'getStoreProfile']);
+Route::get('get-product-detail/{id}',[ProductController::class,'getProductDetail']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -40,7 +43,10 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('category',[CategoryController::class,'getCategories']);
     Route::get('subcategory/{parent_id}',[CategoryController::class,'getSubCategories']);
 
-    // Productsm add to seperate auth group later !
+    // Products add to seperate auth group later !
     Route::post('add-product',[ProductController::class,'add']);
+
+    //add to cart only for login user
+    Route::post('add-cart',[CartController::class,'addCart']);
 });
 
