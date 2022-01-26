efa97a3d9433f755e0c89e90ccdbc507d61ed0bc
adioihsan <adioihsan@gmail.com>
2022-01-26 19:21:10 +0700

    Add inital data to seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(UsersTableSeeder::class);
+        $this->call(UserProfilesTableSeeder::class);
+        $this->call(UserAddressesTableSeeder::class);
+        $this->call(StoreProfilesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(ProductReviewsTableSeeder::class);
+        $this->call(PersonalAccessTokensTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+        $this->call(OrderItemsTableSeeder::class);
+        $this->call(FmoodPaysTableSeeder::class);
+        $this->call(CartsTableSeeder::class);
     }
 }
